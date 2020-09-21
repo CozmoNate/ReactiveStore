5223877ad5b1a17809fe796275f54e54270da8fa
Natan Zalkin <natan.zalkin@me.com>
2020-09-21 15:07:27 +0300

    Lower package requirements

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,10 +6,10 @@ import PackageDescription
 let package = Package(
     name: "ReactiveStore",
     platforms: [
-        .iOS(.v12),
-        .macOS(.v10_14),
-        .watchOS(.v5),
-        .tvOS(.v12)
+        .iOS(.v10),
+        .macOS(.v10_12),
+        .watchOS(.v4),
+        .tvOS(.v10)
     ],
     products: [
         .library(name: "ReactiveStore",
